b6a30079c9249b58f727e26680576a4b8d150688
Peter <peterogb067@gmail.com>
2025-10-09 21:59:48 +0100

    Add .env and Update App.js

--- weather-dashboard/src/App.jsx
+++ weather-dashboard/src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import WeatherCard from "./components/WeatherCard";
 
-function App() {
-  const [count, setCount] = useState(0)
+function WeatherApp() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+
+  const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
+
+  const fetchWeather = async () => {
+    if (!city) return;
+
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_OPENWEATHER_API_KEY}&units=metric`
+      );
+      const data = await res.json();
+      if (data.cod === 200) {
+        setWeather(data);
+      } else {
+        setWeather(null);
+        alert("City not found!");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex justify-center items-center min-h-screen bg-sky-100">
+      <div className="bg-sky-50 rounded-2x1 shadow-md p-6 w-96">
+        <h1 className="text-2x1 font-bold mb-4">Weather Dashboard</h1>
+
+        {/* Search Bar */}
+        <div className="flex mb-6">
+          <input 
+           type="text"
+           placeholder="Search City"
+           value={city}
+           onChange={(e) => setCity(e.target.value)}
+           className="flex-1 p-2 rounded-1-1g border-gray-300 focus:outline-none"
+          />
+          <button
+           onClick={fetchWeather}
+           className="bg-blue-500 text-white px-4 py-2 rounded-r-1g hover:bg-blue-600"
+          >
+            Search 
+          </button>
+        </div>
+
+        {/* Weather Display */}
+        {weather && <WeatherCard data={weather} />}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
